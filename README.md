97f6ca5b3cfc978a02909926cdecd8ff0e474e82
denmj <denismj87@gmail.com>
2021-02-08 21:59:06 -0600

    weather update

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # weather-app
 Simple weather app in python 
 
-weather_api = 3860a5e3975d1317258172dd2623ac4d
+weather_api_key = 3860a5e3975d1317258172dd2623ac4d
+
+news_api_key = 0aa18f0aee7a40d18225c0ed63656d68
--- tkinter-gui.py
+++ tkinter-gui.py
@@ -1,41 +1,19 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import font
+from tkinter import messagebox
 import requests, json
 import time
 from datetime import datetime
+import blpapi
 
 
+def test_news():
+    type = 'sports'
+    apiKey = '0aa18f0aee7a40d18225c0ed63656d68'
+    BASE_URL = f'http://newsapi.org/v2/top-headlines?country=in&category={type}&apiKey='+apiKey
 
-#
-# def get_weather_data(city, units):
-#
-#     weather_url = "https://api.openweathermap.org/data/2.5/weather?"
-#     weather_api = "3860a5e3975d1317258172dd2623ac4d"
-#     url = weather_url + "q=" + city + "&units=" + units + "&appid=" + weather_api
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         print("Getting weather data...")
-#         data = response.json()
-#         main = data['main']
-#         weather = data['weather']
-#
-#         temperature = main['temp']
-#         humidity = main['humidity']
-#         pressure = main['pressure']
-#         report = data['weather']
-#         condition = weather[0]['main']
-#
-#         print(f"{city:-^30}")
-#         print(f"Temperature: {temperature}")
-#         print(f"Humidity: {humidity}")
-#         print(f"Pressure: {pressure}")
-#         print(f"Weather Report: {condition}")
-#         return city, temperature, humidity, pressure, condition
-#     else:
-#         # showing the error message
-#         print("Error in the HTTP request")
-#
+
 
 
 class Mirror_GUI():
@@ -53,10 +31,16 @@ class Mirror_GUI():
     def base_GUI(self):
 
         # Weather frame
-        self.weather_frame = Frame(self.mirror_gui, borderwidth=0, relief="ridge", width=400, height=200, bg='black', highlightbackground="gray")
 
-        self.label_city_time = Label(self.weather_frame, text="City", fg='white', bg='black',
+        # Time
+        self.weather_frame = Frame(self.mirror_gui, borderwidth=5, relief="ridge", width=400,
+                                   height=200, bg='black', highlightbackground="gray")
+
+        # Weather
+        self.label_time = Label(self.weather_frame, text="City", fg='white', bg='black',
                        font=self.normalFont, justify=LEFT)
+        self.label_city = Label(self.weather_frame, text="Loading...", fg='white', bg='black',
+                       font=self.normalFont, justify=CENTER)
         self.label_temperature = Label(self.weather_frame, text="Loading...", fg='white', bg='black',
                         font=self.normalFont, justify=LEFT)
         self.label_humidity = Label(self.weather_frame, text="Loading...", fg='white', bg='black',
@@ -66,15 +50,31 @@ class Mirror_GUI():
         self.label_condition = Label(self.weather_frame, text="Loading...", fg='white', bg='black',
                         font=self.normalFont, justify=LEFT)
 
+        self.label_city.grid(row=0, column=1, sticky=W, pady=2)
+        self.label_time.grid(row=1, column=1, sticky=W, pady=2)
+        self.label_temperature.grid(row=2, column=1, sticky=W, pady=2)
+        self.label_humidity.grid(row=3, column=1, sticky=W, pady=2)
+        self.label_preassure.grid(row=4, column=1, sticky=W, pady=2)
+        self.label_condition.grid(row=5, column=1, sticky=W, pady=2)
 
+        # News frame
+        self.news_frame = Frame(self.mirror_gui, borderwidth=5, relief="ridge", width=400,
+                                   height=200, bg='black', highlightbackground="gray")
+
+        self.mid_frame = Frame(self.mirror_gui, borderwidth=5, relief="ridge", width=1050,
+                                height=200, bg='black', highlightbackground="gray")
+
+
+        # Frame grids
         self.weather_frame.grid(row=0, column=1, sticky=W, padx=20, pady=20)
-        self.label_city_time.grid(row=0, column=1, sticky=W, pady=2)
-        self.label_temperature.grid(row=1, column=1, sticky=W, pady=2)
-        self.label_humidity.grid(row=2, column=1, sticky=W, pady=2)
-        self.label_preassure.grid(row=3, column=1, sticky=W, pady=2)
-        self.label_condition.grid(row=4, column=1, sticky=W, pady=2)
 
-    def get_weather_data(self, city, units):
+        self.mid_frame.grid(row=0, column=2, sticky=W, padx=20, pady=20)
+        self.news_frame.grid(row=0, column=3, sticky=W, padx=20, pady=20)
+
+
+
+    @staticmethod
+    def get_weather_data(city, units):
         weather_url = "https://api.openweathermap.org/data/2.5/weather?"
         weather_api = "3860a5e3975d1317258172dd2623ac4d"
         url = weather_url + "q=" + city + "&units=" + units + "&appid=" + weather_api
@@ -101,29 +101,28 @@ class Mirror_GUI():
             # showing the error message
             print("Error in the HTTP request")
 
-    def update_display(self):
+    def update_time(self):
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
+        current_time_ = "Current time is : " + current_time
+        self.label_time.configure(text=current_time_)
+        self.mirror_gui.after(1000, self.update_time)
 
+    def update_weather(self):
         city, temp, hum, pres, cond = self.get_weather_data("Chicago", "metric")
-
-        current_time_in_city = "Current time in " + city + ": " + current_time
         current_temp = "Temperature: " + str(temp) + " °C"
         current_hum = "Humudity: " + str(hum)
         current_press = "Pressure: " + str(pres)
         current_cond = "Condition: " + cond
-        self.label_city_time.configure(text=current_time_in_city)
+        self.label_city.configure(text=city)
         self.label_temperature.configure(text=current_temp)
         self.label_humidity.configure(text=current_hum)
         self.label_preassure.configure(text=current_press)
         self.label_condition.configure(text=current_cond)
+        self.mirror_gui.after(5000000, self.update_weather)
 
-
-        self.mirror_gui.after(500000, self.update_display)
-
-
-
-
+    def update_news_feed(self):
+        pass
 
 
 
@@ -133,7 +132,8 @@ class Mirror_GUI():
 
 GUI = Mirror_GUI()
 GUI.base_GUI()
-GUI.update_display()
+GUI.update_time()
+GUI.update_weather()
 GUI.display()
 
 
